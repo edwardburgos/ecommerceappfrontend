01f58c82fe294acb8e7655a56d02591b8a590d61
EdwardBurgos <edwardpbn@gmail.com>
2021-12-05 18:20:59 -0500

    react keys implentes in slides map method | improved slides styles

--- src/components/Home/subcomponents/Slides/Slides.tsx
+++ src/components/Home/subcomponents/Slides/Slides.tsx
@@ -3,7 +3,7 @@ import { Carousel } from 'react-bootstrap';
 import s from './Slides.module.css';
 import { SlidesTypes } from '../../../../extras/types';
 
-export default function SlidesQuery() {
+export default function Slides() {
 
     const { loading, error, data } = useSlides();
 
@@ -12,10 +12,10 @@ export default function SlidesQuery() {
     if (error) return null
 
     return (
-        <Carousel>
+        <Carousel className={s.carousel}>
             {
-                data.allSlides.map((e: SlidesTypes) => (
-                    <Carousel.Item>
+                data.allSlides.map((e: SlidesTypes, index: number) => (
+                    <Carousel.Item key={index}>
                         <div>
                             <img
                                 className='d-block w-100'
